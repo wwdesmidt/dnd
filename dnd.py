52d221c12c66978df6d19b159499fe5daad9f246
wwdesmidt <wade.desmidt@gmail.com>
2018-10-05 23:09:29 -0700

    added some of the basic calculations

--- dnd.py
+++ dnd.py
@@ -17,13 +17,77 @@ def printCharacterTitle():
 
 #ability scores
 #what you rolled when you created your character
-#need to add more based on bonuses etc.
-char_base_str = 10
-char_base_dex = 15
-char_base_con = 12
-char_base_int = 15
-char_base_wis = 10
-char_base_cha = 10
+#or add more based on bonuses etc?
+
+ability_scores = {
+	"STR":11,
+	"DEX":15,
+	"CON":13,
+	"INT":20,
+	"WIS":11,
+	"CHA":11,
+}
+
+ability_proficiencies = {
+	"STR":0,
+	"DEX":0,
+	"CON":0,
+	"INT":1,
+	"WIS":1,
+	"CHA":0,
+}
+
+#level as index
+proficiency_bonus_table = [0,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6]
+
+def proficiencyBonus():
+	return proficiency_bonus_table[char_level]
+	
+def abilityProficiencyBonus(ability):
+	return proficiencyBonus()*ability_proficiencies[ability]
+	
+	
+#ability modifier table
+ability_modifier_table = [0,-5,-4,-4,-3,-3,-2,-2,-1,-1,0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10]
+
+def abilityModifier(ability):
+	return ability_modifier_table[ability_scores[ability]]
+
+def abilitySavingThrow(ability):
+	return abilityModifier(ability)+abilityProficiencyBonus(ability)
+	
+	
+#dict of skills and the proficiencies that they use
+#proficiencies match other dicts
+skills = {
+	"Athletics":"STR",
+	"Acrobatics":"DEX",
+	"Sleight of Hand":"DEX",
+	"Stealth":"DEX",
+	"Arcana":"INT",
+	"History":"INT",
+	"Investigation":"INT",
+	"Nature":"INT",
+	"Religion":"INT",
+	"Animal Handling":"WIS",
+	"Insight":"WIS",
+	"Medicine":"WIS",
+	"Perception":"WIS",
+	"Survival":"WIS",
+	"Deception":"CHA",
+	"Intimidation":"CHA",
+	"Performance":"CHA",
+	"Persuasion":"CHA"
+}
+
+def printAbilities():
+	for k, v in ability_scores.items():
+		print("{} score: {}, modifier: {}, proficiency bonus: {}, saving throw: {}".format(k, v, abilityModifier(k), abilityProficiencyBonus(k),abilitySavingThrow(k)))
+	return
+	
+printAbilities()
+	
+print(abilityModifier("INT"))
 
 #ability modifier based on table
 #saving throw = modified + proficience
@@ -37,12 +101,7 @@ def hitDice():
 	#dice_count = hit_dice_per_level*char_level
 	return str(hit_dice_per_level*char_level)+hit_dice
 	
-#level as index
-proficiency_bonus_table = [0,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6]
 
-def proficiencyBonus():
-	#calculate proficiency bonus
-	return proficiency_bonus_table[char_level]
 
 
 #passive perception = 10 + wisdom modifier
